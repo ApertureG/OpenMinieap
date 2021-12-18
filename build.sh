7727c8eef8a6d86c6c37520043cea7fe57b4b7be
Aperture <6523942+xmhio@users.noreply.github.com>
2021-12-18 21:06:35 +0800

    Update

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 wget https://archive.openwrt.org/chaos_calmer/15.05.1/x86/64/OpenWrt-SDK-15.05.1-x86-64_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
 tar -xf OpenWrt-SDK-15.05.1-x86-64_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
 cd OpenWrt-SDK-15.05.1-x86-64_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64
-git clone --recursive https://github.com/ApertureG/OpenMinieap.git
+git clone https://github.com/ApertureG/minieap-openwrt.git package/minieap
+git clone https://github.com/ApertureG/luci-app-minieap.git package/luci-app-minieap
 git clone https://github.com/openwrt-dev/po2lmo.git
-cp -r OpenMinieap/package/* package
 pushd po2lmo
 make && sudo make install
 popd
 make defconfig
 make package/minieap/compile V=s
 make package/luci-app-minieap/compile V=s
+find . -name *.ipk
